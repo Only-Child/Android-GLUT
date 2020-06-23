efdf6455a086ef5897efeb7a7bab87d404a537b0
joker <1012432310@qq.com>
2020-06-23 17:00:56 +0800

    更改点

--- WeChat/src/main/java/com/example/wechat/MainActivity.java
+++ WeChat/src/main/java/com/example/wechat/MainActivity.java
@@ -49,9 +49,7 @@ public class MainActivity extends Activity {
                 case R.id.person:
                     f = new Person_Fragment();//创建第四个Fragment
                     break;
-
                 default:
-
                     break;
             }
             ft.replace(R.id.fragment, f); //替换Fragment
