aee15c7d93ca0ee8c5838e4c84434e12240cb485
Only-Child <156974911@qq.com>
2020-06-25 22:30:37 +0800

    16:12

--- WeChat/src/main/java/com/example/wechat/Home_Fragment.java
+++ WeChat/src/main/java/com/example/wechat/Home_Fragment.java
@@ -7,7 +7,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -25,7 +29,10 @@ import com.example.wechat.utils.StaggeredGridAdapter;
  * Created by Administrator on 2016/2/18.
  */
 public class Home_Fragment extends Fragment {
-
+    private ViewFlipper flipper;
+    private int[] images = new int[]{R.drawable.img1, R.drawable.img2, R.drawable.img3,
+            R.drawable.img4, R.drawable.img5, R.drawable.img6, R.drawable.img7, R.drawable.img8};  //定义图片数组
+    private Animation[] animations=new Animation[2];  //定义两个切换动画动画
 
     private RecyclerView recyclerView;
 
@@ -83,5 +90,24 @@ public class Home_Fragment extends Fragment {
             int gap1=getResources().getDimensionPixelSize(R.dimen.dividerHeight3);
             outRect.set(gap,gap,gap,gap);
         }
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        flipper = getActivity().findViewById(R.id.flipper);
+        for (int i=0;i<images.length;i++){    //遍历图片
+            ImageView imageView=new ImageView(getContext());
+            imageView.setImageResource(images[i]);
+            flipper.addView(imageView);
+        }
+
+
+        /**************初始化动画*********************/
+        animations[0]= AnimationUtils.loadAnimation(getContext(),R.anim.slide_in_right);  //右侧平移进入动画
+        animations[1]=AnimationUtils.loadAnimation(getContext(),R.anim.slide_out_left);  //左侧平移退出动画
+        flipper.setInAnimation(animations[0]);  //为flipper设置进入动画
+        flipper.setOutAnimation(animations[1]);  //为flipper设置退出动画
+        flipper.startFlipping();
     }
 }
