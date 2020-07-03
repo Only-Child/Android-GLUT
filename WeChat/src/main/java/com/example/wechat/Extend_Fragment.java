4dc985c8ff2dd4c2db8531b8cc926ad133329347
Only-Child <156974911@qq.com>
2020-07-03 12:14:16 +0800

    6.28

--- WeChat/src/main/java/com/example/wechat/Extend_Fragment.java
+++ WeChat/src/main/java/com/example/wechat/Extend_Fragment.java
@@ -1,24 +1,72 @@
 package com.example.wechat;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2016/2/18.
  */
-public class Extend_Fragment extends Fragment {
+public class Extend_Fragment extends Fragment{
+    private ListView listView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.activity_extend__fragment,null);
+
         return view;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
+        int[] imageId = new int[]{R.drawable.eimg01, R.drawable.eimg02, R.drawable.eimg03,R.drawable.eimg04,R.drawable.eimg05,
+                R.drawable.eimg06,R.drawable.eimg07,R.drawable.eimg08,R.drawable.eimg09,R.drawable.eimg10
+
+        }; // 定义并初始化保存图片id的数组
+//        int[] imageId = new int[]{R.mipmap.eimg01, R.mipmap.eimg02, R.mipmap.eimg03,R.mipmap.eimg04,R.mipmap.eimg05,
+//                R.mipmap.eimg06,R.mipmap.eimg07,R.mipmap.eimg08,R.mipmap.eimg09,R.mipmap.eimg10,
+//
+//        }; // 定义并初始化保存图片id的数组
+//
+//        String[] title = new String[]{"可乐", "森马", "安踏","naike","阿迪达斯","1","2","3","4","5"};
+//        List<Map<String, Object>> listItems = new ArrayList<Map<String, Object>>(); // 创建一个l
+//        for (int i=0;i<imageId.length;i++){
+//            Map<String,Object>map=new HashMap<String, Object>(); // 实例化Map对象
+//            map.put("image",imageId[i]);
+//            map.put("店铺",title[i]);
+//            listItems.add(map);
+//        }
+//        SimpleAdapter adapter=new SimpleAdapter(getContext(),listItems,
+//                R.layout.main,new String[]{"image"},new int[]{R.id.image}
+//        );
+//        listView.setAdapter(adapter);
+/********************新加***************/
+List<Map<String,Object>>listitem=new ArrayList<Map<String,Object>>();
+for (int i=0;i<imageId.length;i++)
+    {
+          Map<String,Object>map=new HashMap<String, Object>();
+            map.put("image",imageId[i]);
+            listitem.add(map);
+    }
+
+    SimpleAdapter adapter=new SimpleAdapter(getActivity().getApplicationContext(),listitem,R.layout.extend,new String[]{"image"},new int[]{R.id.image});
+        listView=getActivity().findViewById(R.id.list);
+        listView.setAdapter(adapter);
+
+    }
 
 
 }
--- WeChat/src/main/java/com/example/wechat/Person_Fragment.java
+++ WeChat/src/main/java/com/example/wechat/Person_Fragment.java
@@ -8,23 +8,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.TextView;
 import android.widget.Toast;
-
+import android.content.DialogInterface;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by Administrator on 2016/2/18.
- */
+
 public class Person_Fragment extends Fragment {
     SharedPreferences sp;
     SimpleAdapter adapter;
@@ -33,7 +29,7 @@ public class Person_Fragment extends Fragment {
 
 
     int[] imageId = new int[]{R.drawable.img01, R.drawable.img02, R.drawable.img03,
-            R.drawable.img04, R.drawable.img05, R.drawable.img06,
+            R.drawable.img04
 
     }; // 定义并初始化保存图片id的数组
 
@@ -42,8 +38,6 @@ public class Person_Fragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.activity_person__fragment,null);
-//         sp=getContext().getSharedPreferences("mrsoft",getContext().MODE_PRIVATE);
-
         return view;
     }
 
@@ -55,19 +49,20 @@ public class Person_Fragment extends Fragment {
 
         ListView listView=getActivity().findViewById(R.id.listview);
 
-        String[] title = new String[]{"账号："+getUser, "修改密码", "浏览历史", "退出账号","添加1","添加2"}; // 定义并初始化保存列表项文字的数组
+        String[] title = new String[]{"账号："+getUser, "修改密码", "浏览历史", "退出账号"}; // 定义并初始化保存列表项文字的数组
         List<Map<String, Object>> listItems = new ArrayList<Map<String, Object>>(); // 创建一个list集合
         // 通过for循环将图片id和列表项文字放到Map中，并添加到list集合中
         for (int i = 0; i < imageId.length; i++) {
             Map<String, Object> map = new HashMap<String, Object>(); // 实例化Map对象
             map.put("image", imageId[i]);
-            map.put("名字", title[i]);
+            map.put("item", title[i]);
             listItems.add(map); // 将map对象添加到List集合中
         }
 
         adapter = new SimpleAdapter(getContext(), listItems,
-                R.layout.main, new String[] { "名字", "image" }, new int[] {
+                R.layout.main, new String[] { "item", "image" }, new int[] {
                 R.id.title, R.id.image }); // 创建SimpleAdapter
+
         listView.setAdapter(adapter); // 将适配器与ListView关联
 /*************************点击事件******************/
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -76,7 +71,7 @@ public class Person_Fragment extends Fragment {
                 Map<String, Object> map = ( Map<String, Object> )parent.getItemAtPosition(position); 	//获取选择项的值
                 switch (position){
                     case 0:
-                        Toast.makeText(getActivity(),map.get("名字").toString(),Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getActivity(),map.get("item").toString(),Toast.LENGTH_SHORT).show();
                         System.out.println(sp.getString("username","")+" "+sp.getString("password",""));
                         break;
 
@@ -91,12 +86,34 @@ public class Person_Fragment extends Fragment {
                         break;
 
                     case 3:
-                        Toast.makeText(getActivity(),map.get("名字").toString(),Toast.LENGTH_SHORT).show();
-                        SharedPreferences.Editor editor = sp.edit();
+                        AlertDialog alertDialog=new AlertDialog.Builder(getContext()).create();
+                        alertDialog.setIcon(R.drawable.out);
+                        alertDialog.setTitle("警告");
+                        alertDialog.setMessage("退出账号会清空您的登入信息，您确定要退出登入吗？");
+                        alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "否", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });//取消按钮
+                        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "是", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                SharedPreferences.Editor editor = sp.edit();
                         editor.remove("username");
-                        editor.remove("password");
-                        editor.commit();
-                        refresh();
+                                editor.remove("password");
+                                editor.commit();
+                                refresh();
+                            }
+                        });//确定按钮
+                        alertDialog.show();//显示对话框
+                        /************/
+//                        Toast.makeText(getActivity(),map.get("名字").toString(),Toast.LENGTH_SHORT).show();
+//                        SharedPreferences.Editor editor = sp.edit();
+//                        editor.remove("username");
+//                        editor.remove("password");
+//                        editor.commit();
+//                        refresh();
                         //System.out.println(sp.getString("username","")+" "+sp.getString("password",""));
                         break;
 
